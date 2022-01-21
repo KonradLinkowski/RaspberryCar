edf7c352b214278178564ee037595590955e4b36
Konrad Linkowski <linkowski.konrad97@gmail.com>
2022-01-21 14:23:38 +0100

    Basic control

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const express = require('express')
+const app = express()
+const { join } = require('path')
 const { Gpio } = require('onoff')
 
 const standBy = new Gpio(17, 'out')
@@ -6,9 +9,23 @@ const ain2 = new Gpio(26, 'out')
 const bin1 = new Gpio(16, 'out')
 const bin2 = new Gpio(20, 'out')
 
-standBy.writeSync(1)
-ain1.writeSync(1)
-bin2.writeSync(1)
+app.get('/on', (req, res) => {
+  standBy.writeSync(1)
+  ain1.writeSync(1)
+  bin2.writeSync(1)
+  res.sendStatus('/')
+})
+
+app.get('/off', (req, res) => {
+  standBy.writeSync(0)
+  ain1.writeSync(0)
+  bin2.writeSync(0)
+  res.redirect('/')
+})
+
+app.use('/', express.static(join(__dirname, 'public')))
+
+app.listen(3000, () => console.log('Server started'))
 
 process.on('SIGINT', handleFailure)
 process.on('beforeExit', handleFailure)
